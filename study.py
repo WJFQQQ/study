a21d44f11e0326cfab4c9f71d75c5ff463d39c5d
wjf <597898719@qq.com>
2017-12-13 10:48:35 +0800

    Variable Rules

--- study.py
+++ study.py
@@ -13,8 +13,20 @@ def hello_world():
 @app.route('/')
 def index():
     return 'Index Page'
-
 @app.route('/hello')
 def hello():
     return 'Hello World'
 #如 index()和hello()函数所示：route()装饰器用于绑定URL
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    #提供用户资料
+    return 'User %s'%username
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    #显示id
+    return 'Post %s'%post_id
+'''
+将可变部分(<variable_name>)添加到URL可以标记这些特殊部分。
+这些特殊部分作为参数传递给函数
+'''
