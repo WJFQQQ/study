1424554f4ed420ab6b0b571945569e4d6e4da443
wjf <597898719@qq.com>
2017-12-13 11:25:36 +0800

    Unique URL/Redirection Behavior

--- study.py
+++ study.py
@@ -30,3 +30,17 @@ def show_post(post_id):
 将可变部分(<variable_name>)添加到URL可以标记这些特殊部分。
 这些特殊部分作为参数传递给函数
 '''
+
+@app.route('/project/')
+def project():
+    return 'the project page'
+@app.route('/about')
+def about():
+    return 'the about page'
+'''
+上述两个函数的不同在于 app.route()末尾有没有斜杠('/')
+1. project函数中，'/project/'类似于系统文件路径，URL定位于尾部的斜杠
+2. about函数中，'/anout'定义了URL，但是没有斜杠.是UNIX-like systems的文件路径。
+   添加'/'-->('/baout/')将会导致URL错误：404 NOT FOUND
+3. 这允许相似得到URL可以工作甚至'/'。URL应该保持独特，避免重复访问相同的地址
+'''
